597ed1045181d11566e4d2bd8d617c87e6921230
1ndy <demeyer3@illinois.edu>
2021-10-14 21:54:31 -0500

    Redirect to static page after search
    Previously, searching for a gene would result in a url like
    '/search/genesymbol' with the symbol being POSTed to the web server.
    Now the search page will redirect to '/info/gene/<sym>' to display
    the page for the gene or an error message. This is better because
    it lets a user link straight to a page for later reference.

--- geneEd/__init__.py
+++ geneEd/__init__.py
@@ -3,9 +3,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    from . import search
+    from . import search, info
 
     app.register_blueprint(search.bp)
+    app.register_blueprint(info.bp)
 
     @app.route('/')
     def index():
--- geneEd/search.py
+++ geneEd/search.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, redirect
 )
 import mysql.connector
 
@@ -12,11 +12,11 @@ def searchSymbol():
         sym = request.form['query']
         cnx = mysql.connector.connect(user='root', passwd='root', database='geneEd')
         cur = cnx.cursor()
-        query = ("SELECT symbol,fullName,locus FROM gene WHERE symbol = '" + sym + "'")
+        query = ("SELECT symbol FROM gene WHERE symbol = '" + sym + "'")
         cur.execute(query)
 
-        for (symbol,name,locus) in cur:
-            return render_template('search_results.html', symbol=symbol, fullName=name, location=locus)
+        for (symbol,) in cur:
+            return redirect('/info/gene/{0}'.format(symbol))# render_template('gene.html', symbol=symbol, fullName=name, location=locus)
         return render_template('no_results.html',symbol=sym)
     else:
         return render_template('search.html')
