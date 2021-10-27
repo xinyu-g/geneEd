4f7c9db1a05bfc95f8ac023c0d3d29f8deef307f
shenganzhang <zshengan@umich.edu>
2021-10-26 19:51:01 -0500

    login and session

--- geneEd/__init__.py
+++ geneEd/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
+from . import search, info, login
 
-def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
-    from . import search, info
 
-    app.register_blueprint(search.bp)
-    app.register_blueprint(info.bp)
+app.register_blueprint(search.bp)
+app.register_blueprint(info.bp)
+app.register_blueprint(login.bp)
+app.config.from_object('geneEd.config')
 
-    @app.route('/')
-    def index():
-        return 'Welcome to GeneEd'
 
-    return app
+@app.route('/')
+def index():
+    return 'Welcome to GeneEd'
+
---
+++ geneEd/login.py
@@ -0,0 +1,69 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, redirect
+)
+import flask
+import geneEd
+import mysql
+import mysql.connector
+
+
+def get_from_form(name):
+    """Get form['name']."""
+    if name not in flask.request.form:
+        print("Empty!")
+        flask.abort(400)
+    return flask.request.form[name]
+
+
+def get_target():
+    """Get ?target=URL."""
+    target = flask.request.args.get('target')
+    if not target:
+        target = '/'
+    return target
+
+
+def login(connection):
+    """Operation for login."""
+    password_form = get_from_form('password')
+    username_form = get_from_form('username')
+
+    if (not password_form) or (not username_form):
+        print("the username or password fields are empty")
+        flask.abort(400)
+
+    cnx = mysql.connector.connect(user='root', passwd='root', database='geneEd')
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM users WHERE username='" + username_form + "'")
+    user = cursor.fetchone()
+    if not user:
+        print("username and password authentication fails")
+        flask.abort(403)
+    print(user)
+    password = user[1]
+    if password == password_form:
+        flask.session[
+            geneEd.app.config['SESSION_COOKIE_NAME']
+        ] = username_form
+    else:
+        print("username and password authentication fails")
+        flask.abort(403)
+
+
+bp = Blueprint('login', __name__, url_prefix='/user')
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def account_login():
+    """For login."""
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        operation = get_from_form('operation')
+        target = get_target()
+        connection = mysql.connector.connect(user='root', passwd='root', database='geneEd')
+
+        if operation == 'login':
+            login(connection)
+
+        return redirect('/')
