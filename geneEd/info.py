f51f68b6e46b04c34cbbb4c277baafd625120649
1ndy <demeyer3@illinois.edu>
2021-11-29 22:24:28 -0600

    Add like button

--- geneEd/info.py
+++ geneEd/info.py
@@ -20,7 +20,18 @@ def showGenePage(sym):
         # single line updates are already transactions so we don't need one here
         cur.execute(update)
         cnx.commit()
-        return render_template('gene.html', symbol=symbol, fullName=name, location=locus, popularity=popularity+1)
+        query = ("SELECT * FROM favorites WHERE userName='{0}' AND symbol='{1}'".format('cs411', symbol))
+        cur.execute(query)
+        try:
+            results = cur.fetchall()
+        except:
+            results = []
+        likeButtonText = ''
+        if len(results) > 0:
+            likeButtonText = 'Liked'
+        else:
+            likeButtonText = 'Like'
+        return render_template('gene.html', symbol=symbol, fullName=name, location=locus, popularity=popularity+1, likeButtonText=likeButtonText)
     elif len(results) > 1:
         # TODO render a page that lets the user choose whihc one they want to view
         return "multple genes"
