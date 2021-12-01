572cd79f4cd556827d0638bd1a36a937187884a6
shenganzhang <zshengan@umich.edu>
2021-12-01 09:15:53 -0600

    update userprofile
    add recommend genes to user profile

--- geneEd/user.py
+++ geneEd/user.py
@@ -16,10 +16,14 @@ def renderProfile():
     cnx = mysql.connector.connect(user='root', passwd='root', database='geneEd')
     cur = cnx.cursor()
     query1 = ("SELECT username, create_time, is_admin FROM users WHERE id = {0}".format(user_id))
-    query2 = ("SELECT symbol, fullName, createDate, popularity FROM users NATURAL JOIN favorites NATURAL JOIN gene WHERE user_id = {0}".format(user_id))
+    query2 = ("SELECT DISTINCT symbol, fullName, createDate, popularity FROM users NATURAL JOIN favorites NATURAL JOIN gene WHERE user_id = {0}".format(user_id))
+    query3 = ("SELECT DISTINCT symbol, fullName, popularity FROM users NATURAL JOIN recommend NATURAL JOIN gene WHERE user_id = {0}".format(user_id))
     cur.execute(query1)
     user_info = cur.fetchall()[0]
     cur.execute(query2)
     user_favs = cur.fetchall()
+    cur.execute(query3)
+    user_rcmd = cur.fetchall()
 
-    return render_template('user_profile.html', user_favs=user_favs, username=user_info[0], create_date=user_info[1], admin=(False if user_info[2] == None else True))
+    return render_template('user_profile.html', user_favs=user_favs, username=user_info[0], create_date=user_info[1],
+                           admin=(False if user_info[2] == None else True), user_rcmd=user_rcmd)
